d895b67dfceac77619ef6e72522671ed4819023e
Hariom <91545175+Hariom-Singh786@users.noreply.github.com>
2021-10-20 15:36:59 +0530

    Create runningSum.java

---
+++ runningSum.java
@@ -0,0 +1,13 @@
+//running sum of 1D - array
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] runningSum= new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            runningSum[i]=0;
+            for(int j=0;j<=i;j++){
+                runningSum[i]+=nums[j];
+            }
+        }return runningSum;
+    }
+}
